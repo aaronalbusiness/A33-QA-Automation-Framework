029d18b1de064405df2f59f366f6907948b7c74f
aaronalbusiness <aaronalbusiness@gmail.com>
2023-03-14 13:59:01 -0400

    Fixed BaseTest.java

--- src/test/java/Homework18.java
+++ src/test/java/Homework18.java
@@ -1,2 +1,31 @@
-public class Homework18 {
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework18 extends BaseTest {
+
+    @Test
+    public void playSongTest() {
+        login("aaronalbusiness@gmail.com", "te$t$tudent");
+        playSong();
+        Assert.assertTrue(isSongPlaying());
+    }
+
+
+    public void playSong() {
+        WebElement playButton = driver.findElement(By.xpath("//span[@data-testid='playbtn']"));
+        WebElement playNextSongButton = driver.findElement(By.xpath("//div[@data-testid=sound-bar-play']"));
+
+        playNextSongButton.click();
+    }
+
+    public boolean isSongPlaying() {
+        WebElement soundBarPlay = driver.findElement(By.xpath("//div[@data-testid=sound-bar-play']"));
+
+        return soundBarPlay.isDisplayed();
+
+    }
+
 }
